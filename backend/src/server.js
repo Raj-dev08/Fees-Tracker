5f93bae21aa8a32cc311acafa2c9fb149a98a59a
unknown <lipikasen79@gmail.com>
2026-01-02 14:48:31 +0530

    added env vars to safely handle cors and network

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
