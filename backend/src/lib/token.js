d18d2d95b76d633d4609ab6615eae2de110e94c1
unknown <lipikasen79@gmail.com>
2026-01-02 15:40:52 +0530

    fixed cookie issue made by lax

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "lax", // Not same site so making sure it works
+    sameSite: "none", // Not same site so making sure it works
     secure: process.env.NODE_ENV !== "development",
   });
 
